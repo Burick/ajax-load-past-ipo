892e8a60cb44a6f96baaca5849686d87b31cf9f1
Burick <cyberburick@gmail.com>
2021-03-27 11:09:04 +0200

    init

---
+++ ajax-load-past-ipo.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Ajax Load Past Ipo
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: Burick
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
